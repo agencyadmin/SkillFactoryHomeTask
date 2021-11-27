ba66c0ab35e9b3437e74ea15a3ea4a047f73002c
Name <osmprobox@gmail.com>
2021-11-27 20:38:24 +0300

    не рабочий код

--- mypet/Program.cs
+++ mypet/Program.cs
@@ -5,17 +5,21 @@ class MainClass
 	public static void Main(string[] args)
 	{
 
-		string MyName = "Jane";
-		byte MyAge = 27;
+		string MyName;
+		byte MyAge;
 		bool HaveIApet = true;
 		double MyShoeSize = 37.5;
-
-		Console.WriteLine("My name is " + MyName);
-		Console.WriteLine("MyAge " + MyAge);
-		Console.WriteLine("Do I have a pet? " + HaveIApet);
-		Console.WriteLine("My shoe size is " + MyShoeSize);
-
-		enum semaphore:short
-	{ Yellow = 200, Green = 300, Red = 100
-	}
+		enum DayofWeek : byte
+		{Sunday, Monday, Tuesday, Wednessday, Thirsday, Friday, Surturday 
+		}
+		
+		Console.WriteLine("What is your name?");
+		MyName = Console.ReadLine();
+		Console.WriteLine("What is your age?");
+		MyAge = (byte)Console.ReadLine();
+		Console.WriteLine("Your name is {0} and your age is {1}", MyName, MyAge);
+		DayofWeek Day = (DayofWeek)Console.ReadLine();
+		Console.WriteLine($"Enter your favorite day of week:{Day}");
+		
+		}
 }
