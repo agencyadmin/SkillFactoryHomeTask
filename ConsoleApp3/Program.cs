e14c759438dfe4f1dacf31ede55630cda32d909c
Name <osmprobox@gmail.com>
2021-11-27 14:43:46 +0300

    Add project files.

---
+++ ConsoleApp3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+
+        string name;
+        string age;
+        string pet;
+        string footsize;
+
+        Console.WriteLine("Как вас зовут?");
+        name = Console.ReadLine();
+        Console.WriteLine("Сколько вам лет?");
+        age = Console.ReadLine();
+        Console.WriteLine("Если ли у вас питомец?");
+        pet = Console.ReadLine();
+        Console.WriteLine("Какой у вас размер ноги?");
+        footsize = Console.ReadLine();
+
+        Console.WriteLine(name);
+        Console.WriteLine(age);
+        Console.WriteLine(pet);
+        Console.WriteLine(footsize);
+        Console.ReadKey();
+    }
+}
