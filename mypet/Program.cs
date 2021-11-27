44460540e3c4fcdf5eaa62ddecb89576c3009b1c
Name <osmprobox@gmail.com>
2021-11-27 20:40:53 +0300

    добавил парсинг дня из строки

--- mypet/Program.cs
+++ mypet/Program.cs
@@ -18,7 +18,7 @@ class MainClass
 		Console.WriteLine("What is your age?");
 		MyAge = (byte)Console.ReadLine();
 		Console.WriteLine("Your name is {0} and your age is {1}", MyName, MyAge);
-		DayofWeek Day = (DayofWeek)Console.ReadLine();
+		DayofWeek Day = (DayofWeek)int.Parse(Console.ReadLine());
 		Console.WriteLine($"Enter your favorite day of week:{Day}");
 		
 		}
