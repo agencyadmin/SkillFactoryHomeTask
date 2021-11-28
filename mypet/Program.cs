59863ef3a889f7313883812c8a5f2647ff590513
Name <osmprobox@gmail.com>
2021-11-28 13:18:04 +0300

    Финальная работа Модуля тип данных Анкетируем пользователей

--- mypet/Program.cs
+++ mypet/Program.cs
@@ -4,20 +4,25 @@ class MainClass
 {
 	public static void Main(string[] args)
 	{
-
 		string MyName;
 		byte MyAge;
-		enum DayOfWeek : byte
-		{Sunday, Monday, Tuesday, Wednessday, Thirsday, Friday, Surturday 
-		}
-		
-		Console.WriteLine("What is your name?");
+		enum DayOfWeek
+	{ 
+	Sunday = 7,
+	Monday = 1, 
+	Thuesday = 2, 
+	Wednesday = 3, 
+	Thursday = 4,
+	Friday = 5, 
+	Saturday =6
+	}
+	Console.WriteLine("What is your name?");
 		MyName = Console.ReadLine();
 		Console.WriteLine("What is your age?");
-		MyAge = (byte)Console.ReadLine();
-		Console.WriteLine("Your name is {0} and your age is {1}", MyName, MyAge);
-		DayOfWeek Day = (DayOfWeek)int.Parse(Console.ReadLine());
-		Console.WriteLine($"Enter your favorite day of week:{Day}");
-		
-		
+		MyAge = (byte) int.parse(Console.ReadLine());
+	Console.WriteLine("Your name is {0} and your age is {1}", MyName, MyAge);
+		DayOfWeek Day = DayOfWeek.((DayOfWeek)int.Parse(Console.ReadLine()));
+	Console.WriteLine($"Enter your favorite day of week:{Day}");
+	}	
 }
+ 
