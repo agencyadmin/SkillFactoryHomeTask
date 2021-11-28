167fcf1a6dafbf9895bcd3ab8c71bafcda7df45b
Name <osmprobox@gmail.com>
2021-11-28 11:22:35 +0300

    Rename DayOfWeek

--- mypet/Program.cs
+++ mypet/Program.cs
@@ -7,9 +7,7 @@ class MainClass
 
 		string MyName;
 		byte MyAge;
-		bool HaveIApet = true;
-		double MyShoeSize = 37.5;
-		enum DayofWeek : byte
+		enum DayOfWeek : byte
 		{Sunday, Monday, Tuesday, Wednessday, Thirsday, Friday, Surturday 
 		}
 		
@@ -18,8 +16,8 @@ class MainClass
 		Console.WriteLine("What is your age?");
 		MyAge = (byte)Console.ReadLine();
 		Console.WriteLine("Your name is {0} and your age is {1}", MyName, MyAge);
-		DayofWeek Day = (DayofWeek)int.Parse(Console.ReadLine());
+		DayOfWeek Day = (DayOfWeek)int.Parse(Console.ReadLine());
 		Console.WriteLine($"Enter your favorite day of week:{Day}");
 		
-		}
+		
 }
